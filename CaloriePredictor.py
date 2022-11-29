9473412d4941ba415daa29338d1dcaa2a7a26942
Pedro Chaves <106980009+pmpmchaves@users.noreply.github.com>
2022-11-29 22:04:27 +1100

    Update CaloriePredictor.py

--- CaloriePredictor.py
+++ CaloriePredictor.py
@@ -6,3 +6,15 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
 from sklearn import metrics
+
+# Data Collection and Processing
+calories_df = pd.read_csv("/calories.csv")
+calories_df.head()
+exercise_df = pd.read_csv("/exercise.csv")
+exercise_df.head()
+df = pd.concat([exercise_df,calories_df["Calories"]],axis=1)
+df.head()
+df.shape # (15000, 9)
+df.info()
+df.isnull().sum() # There are 0 columns with null values
+
