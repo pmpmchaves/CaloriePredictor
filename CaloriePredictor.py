6b10c2dd512b93bd18eba323abedec141b0af204
pmpmchaves <pmpmchaves@gmail.com>
2022-12-02 21:16:39 +1100

    Path correction

--- CaloriePredictor.py
+++ CaloriePredictor.py
@@ -3,14 +3,13 @@ import numpy as np
 import pickle
 
 # Loading the saved model
-loaded_model = pickle.load(open('C:/Users/Pedro/CalorieCounterProject/trained_model.pkl', 'rb'))
+loaded_model = pickle.load(open('/mnt/c/Users/Pedro/CalorieCounterProject/trained_model.pkl', 'rb'))
 
 # Adjusting our model prediction framework to work with out loaded model
 user_input = (0,27,176,76,25,120,40)
 
 # Transforming user input data into a numpy array
 user_input_array = np.asarray(user_input)
-
 
 # We need to reshape the array so that we can use it in our model
 user_input_reshaped = user_input_array.reshape(1,-1)
